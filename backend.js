c142ce0568fd6b0ea6e0eb69eb3a54e4cd3cfbf1
kreenatotala <74081275+kreenatotala@users.noreply.github.com>
2025-10-08 19:11:20 -0400

    created get recipes route

--- backend.js
+++ backend.js
@@ -1,6 +1,13 @@
 import express from 'express';
-const app = express();
+import recipesRouter from './routes/recipes.js';
+
+console.log("Initalizing backend server...");
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static('frontend'));
+
+app.use('/api/', recipesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
